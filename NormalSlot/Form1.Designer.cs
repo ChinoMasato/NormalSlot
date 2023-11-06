ef2e75b456dcdbb8f5cb93abe6500842a9e4c8e1
Chino Masato <56014502+ChinoMasato@users.noreply.github.com>
2023-11-06 13:31:25 +0900

    slot

--- NormalSlot/Form1.Designer.cs
+++ NormalSlot/Form1.Designer.cs
@@ -35,9 +35,16 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -72,6 +79,7 @@
             // 
             // timer1
             // 
+            this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // button1
@@ -84,20 +92,73 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(150, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ストップ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(371, 377);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "ストップ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(584, 377);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "ストップ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(45, -8);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(696, 167);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(45, 344);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(696, 120);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,6 +171,11 @@
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
     }
 }
 
--- NormalSlot/Form1.cs
+++ NormalSlot/Form1.cs
@@ -12,19 +12,106 @@ namespace NormalSlot
 {
     public partial class Form1 : Form
     {
+        bool reel1;//リールが動いているか止まっているか
+        bool reel2;//リールが動いているか止まっているか
+        bool reel3;//リールが動いているか止まっているか
+        bool stop1;//リール１のストップボタンが押されているか
+        bool stop2;//リール２のストップボタンが押されているか
+        bool stop3;//リール３のストップボタンが押されているか
+        int count1;
+        int count2;
+        int count3;
+
         public Form1()
         {
             InitializeComponent();
+            reel1 = false;//リール停止状態
+            reel2 = false;//リール停止状態
+            reel3 = false;//リール停止状態
+            timer1.Enabled = true;
+            stop1 = false;//ストップボタン押されていない
+            stop2 = false;//ストップボタン押されていない
+            stop3 = false;//ストップボタン押されていない
+            count1 = 0;
+            count2 = 0;
+            count3 = 0;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            pictureBox1.Top++;
+            //リールを止めるか否か
+            if(stop1 && count1 % 4 == 0)
+            {
+                stop1 = false;
+                reel1 = false;
+            }
+            if (stop2 && count2 % 4 == 0)
+            {
+                stop2 = false;
+                reel2 = false;
+            }
+            if (stop3 && count3 % 4 == 0)
+            {
+                stop3 = false;
+                reel3 = false;
+            }
+
+
+            if (reel1)
+            {
+                pictureBox1.Top+=16;
+                count1++;
+                if (pictureBox1.Top>-1197+64*21)
+                {
+                    pictureBox1.Top = -1197;
+                    count1 = 0;
+                }
+            }
+            if (reel2)
+            {
+                pictureBox2.Top+=16;
+                count2++;
+                if (pictureBox2.Top > -1197 + 64 * 21)
+                {
+                    pictureBox2.Top = -1197;
+                    count2 = 0;
+                }
+            }
+            if (reel3)
+            {
+                pictureBox3.Top+=16;
+                count3++;
+                if (pictureBox3.Top > -1197 + 64 * 21)
+                {
+                    pictureBox3.Top = -1197;
+                    count3 = 0;
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            timer1.Enabled = true;
+            reel1 = true;//リール回転状態
+            reel2 = true;//リール回転状態
+            reel3 = true;//リール回転状態
+            stop1 = false;
+            stop2 = false;
+            stop3 = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stop1 = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stop2 = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            stop3 = true;
         }
     }
 }
