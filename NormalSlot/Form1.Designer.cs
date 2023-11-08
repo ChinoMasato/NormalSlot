327bc395b1f35a70431e69fc3af4052f60f8d7e6
Chino Masato <56014502+ChinoMasato@users.noreply.github.com>
2023-11-08 15:01:52 +0900

    Win

--- NormalSlot/Form1.Designer.cs
+++ NormalSlot/Form1.Designer.cs
@@ -40,6 +40,8 @@
             this.button4 = new System.Windows.Forms.Button();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -138,11 +140,33 @@
             this.pictureBox5.TabIndex = 5;
             this.pictureBox5.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(662, 417);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(721, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -176,6 +200,8 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- NormalSlot/Form1.cs
+++ NormalSlot/Form1.cs
@@ -12,26 +12,93 @@ namespace NormalSlot
 {
     public partial class Form1 : Form
     {
-        struct Reel
+        class Reel
         {
             public bool active;//動いているか
             public bool stop;//ストップボタンが押されている
             public int count;//カウンター
+            public int[] graph;//絵柄
+            public int ReelGraphNo(int line)
+            {
+                if(graph.Length==0)
+                {
+                    return -1;
+                }
+                return graph[(int)(count / 4)+line];
+            }
+
         }
         Reel[] reel = new Reel[3];
-
+        int credit;//コイン
 
         public Form1()
         {
             InitializeComponent();
             timer1.Enabled = true;
 
-            for(int i=0; i<reel.Length; i++)
+            for (int i = 0; i < reel.Length; i++)
             {
+                reel[i] = new Reel();
                 reel[i].active = false;
                 reel[i].stop = false;
                 reel[i].count = 0;
+                reel[i].graph = new int[24] { 3,6,1,2,3,2,5,0,2,3,2,4,6,2,3,2,0,5,2,3,2,3,6,1};
+                /*
+                 { 1, 6, 3, 2, 3, 2, 5, 0, 2, 3, 2, 6, 4, 2, 3, 2, 0, 5, 2, 3, 2, 1, 6, 3 };
+                    reel[i].graph[0] = 1;
+                    reel[i].graph[1] = 6;
+                    reel[i].graph[2] = 3;
+                    reel[i].graph[3] = 2;
+                    reel[i].graph[4] = 3;
+                    reel[i].graph[5] = 2;
+                    reel[i].graph[6] = 5;
+                    reel[i].graph[7] = 0;
+                    reel[i].graph[8] = 2;
+                    reel[i].graph[9] = 3;
+                    reel[i].graph[10] = 2;
+                    reel[i].graph[11] = 6;
+                    reel[i].graph[12] = 4;
+                    reel[i].graph[13] = 2;
+                    reel[i].graph[14] = 3;
+                    reel[i].graph[15] = 2;
+                    reel[i].graph[16] = 0;
+                    reel[i].graph[17] = 5;
+                    reel[i].graph[18] = 2;
+                    reel[i].graph[19] = 3;
+                    reel[i].graph[20] = 2;
+                    reel[i].graph[21] = 1;
+                    reel[i].graph[22] = 6;
+                    reel[i].graph[23] = 3;
+                */
             }
+            credit = 100;//100枚セット
+            label1.Text = credit.ToString();
+        }
+        private bool isAllStop()
+        {
+            for (int i = 0; i < reel.Length; i++) {
+                if (!reel[i].active)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private int isWinNum()
+        {
+            int num = 0;
+            if (!isAllStop()) return 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (reel[0].ReelGraphNo(i) == reel[1].ReelGraphNo(i) &&
+                   reel[1].ReelGraphNo(i) == reel[2].ReelGraphNo(i))
+                {
+                    num++;
+                }
+            }
+
+            return num;
+
         }
         //リール更新メソッド（関数）
         private void reelUpdate(ref Reel reel,ref PictureBox pic)
@@ -41,6 +108,10 @@ namespace NormalSlot
             {
                 reel.stop = false;
                 reel.active = false;
+
+                credit += 10 * isWinNum();
+                label1.Text = credit.ToString();
+                
             }
             else
             {
@@ -76,6 +147,8 @@ namespace NormalSlot
                 reel[i].active = true;//リール回転状態
                 reel[i].stop = false;
             }
+            credit--;
+            label1.Text = credit.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
